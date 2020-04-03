f67f47d945e31d3a4e425340a92a910afe6f187f
addirossi <addirossi@gmail.com>
2020-04-04 03:50:12 +0600

    Add password confirmation

--- web/test_api/account/serializers.py
+++ web/test_api/account/serializers.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(required=True, min_length=6, write_only=True)
+    password_confirmation = serializers.CharField(required=True, write_only=True, min_length=6)
 
     class Meta:
         model = User
@@ -11,9 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
             'id',
             'username',
             'password',
+            'password_confirmation',
             'first_name',
             'last_name',
         )
+
+    def validate(self, data):
+        password = data['password']
+        password_confirm = data['password_confirmation']
+        if password != password_confirm:
+            raise serializers.ValidationError(_('Wrong password confirmation'))
+        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(username=validated_data['username'], password=validated_data['password'])
